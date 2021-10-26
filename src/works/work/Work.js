d363bb04adff28e4c2eb12bf65f74f75de4cbbd1
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2021-10-26 22:01:45 +0300

    r

--- src/works/work/Work.js
+++ src/works/work/Work.js
@@ -5,7 +5,7 @@ function Work(props) {
     return (
         <div className={styles.work}>
             <div className={styles.img}>
-                <a href={''}><button className={styles.projectView}>VIEW</button></a>
+                <a href={''} className={styles.projectView}>VIEW</a>
             </div>
             <h3>{props.title}</h3>
             <span className={styles.description}>{props.description}</span>
