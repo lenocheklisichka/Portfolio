c745d0f31ac3e75081e4cea08882daf3b6311559
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2022-02-25 20:57:42 +0300

    refactoring

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -9,10 +9,10 @@ function Contacts() {
             <div className={styles.contactsContainer}>
                 <Title text={"Contacts"}/>
                 <form className={styles.form}>
-                    <input  type='text' placeholder='Name ...'/>
+                    <input type='text' placeholder='Name ...'/>
                     <input type='email' placeholder='Email ...'/>
                     <textarea placeholder='Messages ...' cols={"5"} rows={"8"}/>
-                  <button type='submit' className={styles.buttonForm}>Submit</button>
+                    <button type='submit' className={styles.buttonForm}>Submit</button>
                 </form>
             </div>
         </div>
