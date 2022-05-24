29ef312f3268a248f4f113bba707cfe2d793c0b6
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2022-05-24 16:54:32 +0300

    add link todolist

--- src/works/Works.js
+++ src/works/Works.js
@@ -28,6 +28,7 @@ function Works() {
                           description={"Project Social network with the ability to add posts, subscribe, login. Stack used: TypeScript, React, Redux, Redux Thunk, Axios."}
                           link={'https://lenocheklisichka.github.io/soc-network/'}/>
                     <Work style={todolist} title={"Todolist"}
+                          link={'https://lenocheklisichka.github.io/it-incubator-todolist-ts/'}
                           description={"Project Todolists with the ability to add todolists, tasks. Stack used: TypeScript, React, Redux, Redux Thunk, Axios."}/>
                     <Work style={counter} title={"Counter"}
                           description={"Project Counter. Stack used: TypeScript, React, Reducer, Store"}
